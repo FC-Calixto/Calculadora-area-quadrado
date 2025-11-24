a00845b2a007ff9f07689f3889563354f70e2691
Felipe Calixto <fandre.cali@gmail.com>
2025-11-24 14:58:26 -0300

    Validar entrada: apenas dígitos numéricos, 2 casas decimais  e não nulo. Formatar saída até 2 casas decimais

--- src/CalculaAreaQuadrado.java
+++ src/CalculaAreaQuadrado.java
@@ -3,17 +3,39 @@ import java.util.Scanner;
 public class CalculaAreaQuadrado {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        //Solicitar a medida do lado do quadrado
-        System.out.println("Digite a medida do lado do quadrado: (em m², 2 casas decimais)");
-        int lado = scanner.nextInt();
+        double lado = 0;
+        boolean valido = false;
 
-        //Calcular a área do quadrado
-        int resultado = lado * lado;
+        while (!valido) {
 
-        //Exibir o resultado
-        System.out.println("A área do quadrado é: " + resultado + "m²");
+            //Solicitar a medida lateral do quadrado
+            System.out.println("Digite a medida lateral do quadrado em metros (até 2 casas decimais): ");
+            String entrada = scanner.nextLine();
 
-        scanner.close();
+            //Normalizar entrada com , ou . na medida com casas decimais
+            entrada = entrada.replace(',','.');
+
+            try {
+                //Converter entrada para double
+                lado = Double.parseDouble(entrada);
+
+                //Verifica limite de 2 casas decimais
+                if (entrada.matches("\\d+(\\.\\d{1,2})?") && lado > 0) {
+                    valido = true;
+                } else {
+                    System.out.println("Erro: Informe um número positivo com até duas casas decimais.");
+                }
+            } catch (NumberFormatException e) {
+                    System.out.println("Erro: Entrada inválida. Digite apenas números decimais");
+                }
+        }
+
+            //Calcular a área do quadrado
+            double area = lado * lado;
+
+            //Exibir resultado ao usuário utilizando o %.2f m²%n para exibir apenas 2 casas decimais
+            System.out.printf("Área do quadrado: %.2f m²%n", area);
+
+            scanner.close();
+        }
     }
-}
